b655d1903f4757e4caf708dccd2723383c01ace4
Govind Mishra <50535437+igovind@users.noreply.github.com>
2025-01-25 23:06:42 +0530

    bug fix

--- android/src/main/java/com/razorpay/razorpay_flutter/RazorpayFlutterPlugin.java
+++ android/src/main/java/com/razorpay/razorpay_flutter/RazorpayFlutterPlugin.java
@@ -14,7 +14,10 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodCall;
+
 
 /**
  * RazorpayFlutterPlugin
@@ -48,6 +51,7 @@ public class RazorpayFlutterPlugin implements FlutterPlugin, MethodCallHandler, 
 
     @Override
     public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+                channel.setMethodCallHandler(null);
     }
 
 
